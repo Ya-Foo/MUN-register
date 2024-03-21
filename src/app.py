b0b95a747beba4abbc168ec823501dabf83097f1
Gia Phu Huynh <152614483+Ya-Hoo@users.noreply.github.com>
2024-03-21 19:05:41 +0700

    added copyright holder to app

--- src/app.py
+++ src/app.py
@@ -151,7 +151,7 @@ class MainWindow(QMainWindow):
         self.copyrightFrameLayout = QFormLayout()
         self.logoLabel = QLabel()
         self.logoLabel.setPixmap(QPixmap("./icons/logo/white.png").scaledToWidth(width//25))
-        self.copyrightFrameLayout.addRow(self.logoLabel, QLabel("MIT License\n\nCopyright (c) 2024 Gia Phu Huynh"))
+        self.copyrightFrameLayout.addRow(self.logoLabel, QLabel("MIT License Copyright (c) 2024\n\nGia Phu Huynh and Quang Hien Bui"))
         self.copyrightFrameLayout.setFormAlignment(Qt.AlignBottom)
         self.copyrightFrameLayout.setHorizontalSpacing(25)
         self.copyrightFrame.setLayout(self.copyrightFrameLayout)
